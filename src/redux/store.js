b15e3ef75e68623a1ac1597acd65da79bf7baddd
Tanyapavliuk <tanyapavlyk1998@gmail.com>
2023-09-02 21:29:16 +0300

    add persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,34 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import ContactReduser from './sliceContact';
 import FilterReduser from './sliceFilter';
+
+const persistConfig = {
+  key: 'phonebook',
+  storage,
+  blacklist: ['filter'],
+};
 
 const rootReducer = combineReducers({
   contants: ContactReduser,
   filter: FilterReduser,
 });
 
-const store = configureStore({
-  reducer: rootReducer,
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
 });
 
-export default store;
+export const persistor = persistStore(store);
+
+// const store = configureStore({
+//   reducer: rootReducer,
+// });
+
+// export default store;
 
 // function contantsReducer(state = [], action) {
 //   switch (action.type) {
